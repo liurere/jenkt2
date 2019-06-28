031729bec7f0c1f53e08eeb8c8002b04e9cd2b60
liuxiangxian <liu.xiangxian@qq.com>
2019-06-28 18:06:22 +0800

    init

---
+++ src/main/java/com/jenkt2/JenkinsTestApplication.java
@@ -0,0 +1,25 @@
+package com.jenkt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@RestController
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World";
+	}
+}
